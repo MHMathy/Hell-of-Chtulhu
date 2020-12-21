f0538fafa708e39cf2cd08daf6d86dba9b295fe5
François Bouscaillou <francois.bouscaillou@gmail.com>
2020-12-21 23:33:30 +0100

    add test scene

--- src/Unity/Assets/Scripts/PlayerController.cs
+++ src/Unity/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,36 @@
 ﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float SpeedPlayer;
+    public Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Instancie mon Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Z))
+        {
+            playerTransform.position += Vector3.forward * Time.deltaTime * SpeedPlayer;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTransform.position += Vector3.back * Time.deltaTime * SpeedPlayer;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
     }
 }
