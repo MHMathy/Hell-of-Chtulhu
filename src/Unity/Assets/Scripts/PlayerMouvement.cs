b0f1bcba5ea0cf3c414893b7ed558ad398551869
Misames <francois.bouscaillou@gmail.com>
2020-12-26 15:00:41 +0100

    add jump with physics

--- src/Unity/Assets/Scripts/PlayerMouvement.cs
+++ src/Unity/Assets/Scripts/PlayerMouvement.cs
@@ -5,24 +5,30 @@ public class PlayerMouvement : MonoBehaviour
     public CharacterController controller;
     public Transform groundCheck;
     public LayerMask groundMask;
-    private bool isGrounded;
     public float groundDistance = 0.4f;
     public float speed = 12f;
     public float gravity = -9.81f;
     private Vector3 velocity;
+    private bool isGrounded;
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = 0f;
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(5f * -2f * gravity);
+        }
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
