dcc8d2de4e9ecd520c503d72169371cb76d8e6f9
Misames <francois.bouscaillou@gmail.com>
2020-12-26 22:30:42 +0100

    add crosshair UI & gun to player

---
+++ src/Unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+    public float damage = 10f;
+    public Camera FpsCam;
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FpsCam.transform.position, FpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }
+        }
+    }
+}
---
+++ src/Unity/Assets/Scripts/Target.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 100f;
+    public void takeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
