bf71751bf6c60b96a86c6ad9d74a5d79aba7255d
Misames <francois.bouscaillou@gmail.com>
2020-12-26 22:39:52 +0100

    add frameRate & convert script in utf8

--- src/Unity/Assets/Scripts/Gun.cs
+++ src/Unity/Assets/Scripts/Gun.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     public float range = 100f;
     public float damage = 10f;
+    public int fireRate = 5;
     public Camera FpsCam;
+    private float nextTimeToFire = 0f;
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
