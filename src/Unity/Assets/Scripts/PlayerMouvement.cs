5d9fea3067de323abdd5ff6e19b089b5b8d3bc5a
Misames <francois.bouscaillou@gmail.com>
2020-12-26 15:16:50 +0100

    fix jump with physics

--- src/Unity/Assets/Scripts/PlayerMouvement.cs
+++ src/Unity/Assets/Scripts/PlayerMouvement.cs
@@ -14,9 +14,10 @@ public class PlayerMouvement : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        // Check si on est au sol et que le gameobjet soit pas en chute libre
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = 0f;
+            velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -24,9 +25,10 @@ public class PlayerMouvement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
+        // Check si on est bien en contact le sol et qu'on appuis sur la touche de saut
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(5f * -2f * gravity);
+            velocity.y = Mathf.Sqrt(3f * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
