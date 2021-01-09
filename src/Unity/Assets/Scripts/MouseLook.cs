edb2989725a4838897e7369c0e19246e02297f90
Misames <fbouscaillou@myges.fr>
2021-01-09 20:33:10 +0100

    add brume effect blender

--- src/Unity/Assets/Scripts/MouseLook.cs
+++ src/Unity/Assets/Scripts/MouseLook.cs
@@ -5,12 +5,10 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerBody;
     private float xRotation = 0f;
-
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
